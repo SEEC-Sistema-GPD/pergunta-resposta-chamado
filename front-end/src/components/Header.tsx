c144394eccc667b95f31d4076fcc1c3066723580
Raquel Brena <raquel.lima.124@ufrn.edu.br>
2025-02-18 11:04:34 -0300

    chore: add tailwind e radix libs, + mocks das dúvidas de chamado e adição de accordion

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <header className="w-full h-[15vh] bg-[#3D4A7B] text-white ">
+            <div className="bg-[#3D4A7B] h-[40%] border-b-4 border-[#D99C44] items-center justify-center " >
+                <h1 className="text-lg w-full h-full px-3 py-2 font-medium">SEEC - SIGEduc - Sistema Integrado de Gestão da Educação</h1>
+            </div>
+            <div className="h-[60%] bg-[#C4D2EB]">
+                <p className="text-primary p-4">
+                    Perguntas Frequentes para a Equipe de Suporte do SEEC - SIGEduc
+                </p>
+            </div>
+        </header>
+    )
+}
