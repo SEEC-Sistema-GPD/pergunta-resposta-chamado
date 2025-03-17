a6027029f074384b17898bdb116f543459f4b31f
Bruno  Almeida <dev.brunoalmeida@gmail.com>
2025-03-17 16:30:57 -0300

    feat: add token authentication to categorias and respostas routes

--- back-end/src/presentation/routes/CategoriasRoutes.ts
+++ back-end/src/presentation/routes/CategoriasRoutes.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { CategoriaController } from "../controllers/CategoriasController.ts";
+import { authenticateToken } from "../../infra/middlewares/authenticateToken.ts";
 
 const categoriaController = new CategoriaController();
 const router = Router();
 
-router.get('/', async (req, res) => categoriaController.findAll(req, res));
+router.get('/', authenticateToken, async (req, res) => categoriaController.findAll(req, res));
 
-router.get('/:id', async (req, res) => categoriaController.findById(req, res));
+router.get('/:id', authenticateToken, async (req, res) => categoriaController.findById(req, res));
 
-router.post('/', async (req, res) => categoriaController.create(req, res));
+router.post('/', authenticateToken, async (req, res) => categoriaController.create(req, res));
 
-router.put('/:id', async (req, res) => categoriaController.update(req, res));
+router.put('/:id', authenticateToken, async (req, res) => categoriaController.update(req, res));
 
-router.delete('/:id', async (req, res) => categoriaController.delete(req, res));
+router.delete('/:id', authenticateToken, async (req, res) => categoriaController.delete(req, res));
 
 export default router;
